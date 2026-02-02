b9ec04e414edd660a5b4b79352f3718be8530487
Tadeo Asrin <tadeoasrin@gmail.com>
2026-02-02 20:26:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8891359241-1a978",
+  "appId": "1:376143646377:web:9511bff7662a965176a3b8",
+  "apiKey": "AIzaSyB3auD7PpIAuIPN7890saXt80cXr8dkrVU",
+  "authDomain": "studio-8891359241-1a978.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "376143646377"
+};
